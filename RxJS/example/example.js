ed27a5d928dafcf61ebc43e1d6f93deb7d129283
Juphy <1571066810@qq.com>
2018-10-17 19:25:35 +0800

    调整文件结构

--- RxJS/example/example.js
+++ RxJS/example/example.js
@@ -1,4 +1,6 @@
 var btn = document.getElementById('btn');
+
+// 创建流
 Rx.Observable.fromEvent(btn, 'click')
 // .map(e => 1)
 // .scan((a, b) => a + b)
@@ -7,6 +9,7 @@ Rx.Observable.fromEvent(btn, 'click')
         console.log(val);
     });
 
+// 合并流
 Rx.Observable.fromEvent(document.querySelector('input[name="add"]'), 'click')
     .mapTo(1)
     .merge(
@@ -18,8 +21,16 @@ Rx.Observable.fromEvent(document.querySelector('input[name="add"]'), 'click')
         console.log(v);
     });
 
-Rx.Observable.fromEvent(document.body, 'click')
-    .map(a => Rx.Observable.of(1, 2, 3))
+// 扁平化
+Rx.Observable.fromEvent(document.querySelector('#btn1'), 'click')
+    .map(a => Rx.Observable.of(1, 2, 3)) // 返回的是一个Observable
+    .concatAll()
+    .subscribe(v => {
+        console.log(v);
+    });
+
+Rx.Observable.fromEvent(document.querySelector('#btn1'), 'click')
+    .flatMap(a => Rx.Observable.of(1, 2, 3)) // 返回的是一个Observable
     .subscribe(v => {
         console.log(v);
     });
@@ -29,3 +40,11 @@ let dragDOM = document.getElementById('drag'), body = document.body;
 let mouseDown = Rx.Observable.fromEvent(dragDOM, 'mousedown');
 let mouseUp = Rx.Observable.fromEvent(body, 'mouseup');
 let mouseMove = Rx.Observable.fromEvent(body, 'mousemove');
+
+mouseDown
+    .flatMap(e => mouseMove.takeUntil(mouseUp)) // mouseDown的时候监听mouseMove，mouseUp的时候停止监听mouseMove
+    .map(e => ({x: e.clientX, y: e.clientY}))
+    .subscribe(pos => {
+        dragDOM.style.left = pos.x + 'px';
+        dragDOM.style.top = pos.y + 'px';
+    })
